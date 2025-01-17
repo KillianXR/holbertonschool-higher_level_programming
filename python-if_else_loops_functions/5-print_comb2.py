31282dd4da282385088d981702ba34680bc0c761
KillianXR <9756@holbertonstudents.com>
2025-01-17 14:17:45 +0100

    5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for ascii in range(0, 100):
     if ascii < 10:
         print("0{}, ".format(ascii), end="")
     else:
-        print("{}, ".format(ascii), end="")
+        print("{}".format(ascii), end=", " if ascii != 99 else "")
