be844cfd944d8f525d97cce4b82c59706de44408
KillianXR <9756@holbertonstudents.com>
2025-01-17 15:49:05 +0100

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for num1 in range(0, 9):
     for num2 in range(0, 10):
-        if num1 != num2:
-            print(f"{num1}{num2}", end=", ")
-        else:
+        if num1 == num2 or num1 == 1 and num2 == 0:
             continue
+        else:
+            print(f"{num1}{num2}", end=", ")
         if num1 == 8 and num2 == 9:
             print(f"{num1}{num2}", end="")
