cf63380f2f757778c196c025667326a92b4950c7
KillianXR <9756@holbertonstudents.com>
2025-01-23 03:50:33 +0100

    pycodestyle 10-best_score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
-    
+
     bigger_contenair = None
     bigger_containt = 0
-    
+
     for contenair, containt in a_dictionary.items():
         if containt > bigger_containt:
             bigger_containt = containt
             bigger_contenair = contenair
     return bigger_contenair
-           
