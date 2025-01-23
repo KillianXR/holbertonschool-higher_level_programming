6fa42998ca0bd2d197e6816d3fb5b00b785d977e
KillianXR <9756@holbertonstudents.com>
2025-01-23 03:19:57 +0100

    10-best_score.py

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    
+    bigger_contenair = None
+    bigger_containt = 0
+    
+    for contenair, containt in a_dictionary.items():
+        if containt > bigger_containt:
+            bigger_containt = containt
+            bigger_contenair = contenair
+    return bigger_contenair
+           
