e72a8cce67a0b7a16ce1b02c5c114f9f04a30697
KillianXR <9756@holbertonstudents.com>
2025-01-27 19:37:33 +0100

    README.md + comments

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 class Square():
+    """"create a square's class"""
     pass
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 class Square():
+    """initialize a square's class with a private class atribute"""
     def __init__(self, size):
         self.__size = size
