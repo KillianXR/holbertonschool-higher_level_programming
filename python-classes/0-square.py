53b70264820c3cb4180cc43eeead446b325eead3
KillianXR <9756@holbertonstudents.com>
2025-01-27 19:49:27 +0100

    comments

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+
+"""
+This is the Square module who initialize a square class
+"""
+
+
 class Square():
     """"create a square's class"""
     pass
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This is the Square module who initialize a square class,
+with a private class attribute "size"
+"""
+
+
 class Square():
     """initialize a square's class with a private class atribute"""
     def __init__(self, size):
