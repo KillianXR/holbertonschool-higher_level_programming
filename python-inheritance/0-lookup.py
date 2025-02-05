c97b32bff43789a60ecc493301afbb96e6db3f8e
KillianXR <9756@holbertonstudents.com>
2025-02-05 10:36:35 +0100

    1-my_list.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,6 +3,7 @@
 returns the list of available attributes and methods of an object passed in
 """
 
+
 def lookup(obj):
-			"""returns the list of available attributes and methods of an object"""
-			return dir(obj)
+    """returns the list of available attributes and methods of an object"""
+    return dir(obj)
---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Class of list.
+"""
+
+
+class MyList(list):
+    
+    def print_sorted(self):
+        """public instance, that prints the list, but sorted."""
+        print(sorted(self))
