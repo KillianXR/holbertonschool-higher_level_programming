e8b86ef127b3a79ca86ecb55ba5936cff6d5c871
KillianXR <9756@holbertonstudents.com>
2025-02-06 15:21:49 +0100

    10-square.py

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+simple class
+"""
+
+
+class BaseGeometry:
+    """create a class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """check if value is positive integer"""
+        if type(value) is not int:
+            raise TypeError(f"{str(name)} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{str(name)} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """rectangle class"""
+    def __init__(self, width, height):
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        result = f"[Rectangle] {self.__width}/{self.__height}"
+        return result
+
+class Square(Rectangle):
+    """square class"""
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.integer_validator('size', size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
