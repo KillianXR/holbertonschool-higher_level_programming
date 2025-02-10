d37b6e9e9d9e8f93f34cf05605cef931cec39b5e
KillianXR <9756@holbertonstudents.com>
2025-02-10 10:14:09 +0100

    2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -4,6 +4,6 @@
 
 def append_write(filename="", text=""):
     """function that return the len of a folder"""
-    with open(filename, "w", encoding="utf-8") as f:
+    with open(filename, "a", encoding="utf-8") as f:
         f.write(text)
         return len(text)
