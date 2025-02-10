8424fe1263cf5ab93e532ac2ddf38e8696375c8f
KillianXR <9756@holbertonstudents.com>
2025-02-10 10:03:09 +0100

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """function that return the len of a folder"""
+
+
 def write_file(filename="", text=""):
     """function that return the len of a folder"""
     with open(filename, "w", encoding="utf-8") as f:
