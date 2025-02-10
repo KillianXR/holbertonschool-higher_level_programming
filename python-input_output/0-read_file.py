ab08c47951e69c3e440867ed7d857fc992b26729
KillianXR <9756@holbertonstudents.com>
2025-02-10 07:10:34 +0100

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""function that print in the output the content of a folder"""
+
+
 def read_file(filename=""):
+    """function that print in the output the content of a folder"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(),end='')
