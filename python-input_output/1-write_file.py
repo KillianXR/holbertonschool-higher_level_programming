b65a093bf70bf0e79ae258c46d6d287bb4663b71
KillianXR <9756@holbertonstudents.com>
2025-02-10 10:01:24 +0100

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""function that return the len of a folder"""
 def write_file(filename="", text=""):
-    """function that print the number of char of a folder"""
+    """function that return the len of a folder"""
     with open(filename, "w", encoding="utf-8") as f:
         f.write(text)
         return len(text)
