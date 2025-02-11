068b833d660f34ef05c530aeb372340a6e24055b
KillianXR <9756@holbertonstudents.com>
2025-02-11 14:35:48 +0100

    8-class_to_json.py

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+return dictionnary description of an object
+"""
+
+def class_to_json(obj):
+    """
+    return dictionnary description of an object
+    """
+    return obj.__dict__
