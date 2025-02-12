7f9411bcd5d53c763c3b7530ec948193b2286f92
KillianXR <9756@holbertonstudents.com>
2025-02-12 10:31:54 +0100

    9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
+"""
+this module create a class of students with instances,
+and retrieves a dictionary representation of a student instance
+"""
 
 class Student():
+    """class creating"""
 
     def __init__(self, first_name, last_name, age):
+        """
+        class instances
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
