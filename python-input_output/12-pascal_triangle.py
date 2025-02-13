ab8a91738d1a9b43e4dcd503fd4470a36a8b0ad7
KillianXR <9756@holbertonstudents.com>
2025-02-13 08:42:42 +0100

    12-pascal_triangle.py

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    while len(triangle) is not n:
+        new_line = [1]
+
+        for count in range(len(triangle[-1]) - 1):
+            new_line.append(triangle[-1][count] + triangle[-1][count + 1])
+
+        new_line.append(1)
+        triangle.append(new_line)
+
+    return triangle
+    
