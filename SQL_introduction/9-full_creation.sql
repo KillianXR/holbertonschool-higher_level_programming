498eae2aa9f31624873ddffa37086a0c4a01c301
KillianXR <9756@holbertonstudents.com>
2025-03-07 15:04:08 +0100

    9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,8 @@
 -- create seconds_table if not exist with id, name, score row's in it
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT,name VARCHAR(256),score INT);
+
 -- insert records with value in it
-INSERT INTO seconds_table (id, name, score) VALUES (1, 'John', 10);
-INSERT INTO seconds_table (id, name, score) VALUES (2, 'Alex', 3);
-INSERT INTO seconds_table (id, name, score) VALUES (3, 'Bob', 14);
-INSERT INTO seconds_table (id, name, score) VALUES (4, 'George', 8);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
