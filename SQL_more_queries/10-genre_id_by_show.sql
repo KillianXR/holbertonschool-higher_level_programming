a1c9e07ff7f6ed0555083374b1290f3fc6f950da
KillianXR <9756@holbertonstudents.com>
2025-03-11 13:30:58 +0100

    doc

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
---
+-- show tv show with is genre by id 
 SELECT tv_shows.title, tv_genres.id AS genre_id
 FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
